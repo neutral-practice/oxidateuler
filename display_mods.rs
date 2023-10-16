f362a553d44919df7f35c74dd9c9bb0c57152a56
neutral-practice <sixtyfour_marmot@proton.me>
2023-10-16 14:08:58 +0200

    modulated calls

--- display_mods.rs
+++ display_mods.rs
@@ -1,21 +1,47 @@
-pub mod display_mods {
-	
-	pub trait Groupable {
-	    fn group_with_nothing(&self) -> String;
-	}
-	
-	impl Groupable for u128 {
-	    fn group_with_nothing(&self) -> String {
-	        self
-	        .to_string()                             // lol
-	        .as_bytes()                              // this is 
-	        .rchunks(3)                              // how
-	        .rev()                                   // we 
-	        .map(std::str::from_utf8)                // format large numbers
-	        .collect::<Result<Vec<&str>, _>>()       // to visually readable formats
-	        .unwrap()                                // in rust
-	        .join(" ")                               // and nobody minds this
-	    }
-	}
+use std::time::{Duration, SystemTime};
+use std::thread;
 
+pub trait Groupable {
+    fn group_with_nothing(&self) -> String;
+}
+
+impl Groupable for u128 {
+    fn group_with_nothing(&self) -> String {
+        self
+        .to_string()                             // lol
+        .as_bytes()                              // this is 
+        .rchunks(3)                              // how
+        .rev()                                   // we 
+        .map(std::str::from_utf8)                // format large numbers
+        .collect::<Result<Vec<&str>, _>>()       // to visually readable formats
+        .unwrap()                                // in rust
+        .join(" ")                               // and nobody minds this
+    }
+}
+
+pub fn record_nanos() -> u128 {
+	return SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos();
+}
+
+pub fn wait_one_millis_and_micros_and_nanos(){
+    let tsn = Duration::from_nanos(1);
+    // Print text to the console.
+    
+    thread::sleep(tsn);
+
+    let tsn2 = Duration::from_micros(1);
+    // Print text to the console.
+    
+    thread::sleep(tsn2);
+
+    let tsn3 = Duration::from_millis(1);
+    // Print text to the console.
+    
+    thread::sleep(tsn3);
+}
+
+pub fn display_time_elapsed_nice(recorded_start: u128) {
+    println!("Time elapsed since received timestamp: {}", (SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos() - recorded_start)
+    .group_with_nothing()
+    );                                       // this is great
 }
--- monolith.rs
+++ monolith.rs
@@ -4,39 +4,15 @@
 
 // This is the main function.
 
-use std::time::{Duration, SystemTime};
-use std::thread;
-
 mod display_mods;
-use display_mods::display_mods::Groupable;
+use display_mods::{Groupable, wait_one_millis_and_micros_and_nanos, display_time_elapsed_nice, record_nanos};
 
 fn main() {
-    let duration_since_epoch_nanos = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos();
+    let duration_since_epoch_nanos = record_nanos();
     // Statements here are executed when the compiled binary is called.
+
+    println!("welcome to a {} nanoseconds of runtime", 100000000.group_with_nothing());
 
     wait_one_millis_and_micros_and_nanos();
     display_time_elapsed_nice(duration_since_epoch_nanos);
-}
-
-fn wait_one_millis_and_micros_and_nanos(){
-    let tsn = Duration::from_nanos(1);
-    // Print text to the console.
-    
-    thread::sleep(tsn);
-
-    let tsn2 = Duration::from_micros(1);
-    // Print text to the console.
-    
-    thread::sleep(tsn2);
-
-    let tsn3 = Duration::from_millis(1);
-    // Print text to the console.
-    
-    thread::sleep(tsn3);
-}
-
-fn display_time_elapsed_nice(recorded_start: u128) {
-    println!("Hello World! Time passed {}", (SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos() - recorded_start)
-    .group_with_nothing()
-    );                                       // this is great
 }
