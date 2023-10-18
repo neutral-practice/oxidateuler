41f4f25e3fc2fb5b9aa016b54d36b2a20a67d1d4
neutral-practice <sixtyfour_marmot@proton.me>
2023-10-18 17:41:06 +0200

    opening a window on windows

--- src/display_mods.rs
+++ src/display_mods.rs
@@ -1,5 +1,5 @@
-use std::time::{Duration, SystemTime};
 use std::thread;
+use std::time::{Duration, SystemTime};
 
 pub trait Groupable {
     fn group_with_nothing(&self) -> String;
@@ -7,41 +7,56 @@ pub trait Groupable {
 
 impl Groupable for u128 {
     fn group_with_nothing(&self) -> String {
-        self
-        .to_string()                             // lol
-        .as_bytes()                              // this is 
-        .rchunks(3)                              // how
-        .rev()                                   // we 
-        .map(std::str::from_utf8)                // format large numbers
-        .collect::<Result<Vec<&str>, _>>()       // to visually readable formats
-        .unwrap()                                // in rust
-        .join(" ")                               // and nobody minds this
+        self.to_string() // lol
+            .as_bytes() // this is
+            .rchunks(3) // how
+            .rev() // we
+            .map(std::str::from_utf8) // format large numbers
+            .collect::<Result<Vec<&str>, _>>() // to visually readable formats
+            .unwrap() // in rust
+            .join(" ") // and nobody minds this
     }
 }
 
 pub fn record_nanos() -> u128 {
-	return SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos();
+    return SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_nanos();
 }
 
-pub fn wait_one_millis_and_micros_and_nanos(){
+pub fn wait_one_millis_and_micros_and_nanos() {
     let tsn = Duration::from_nanos(1);
     // Print text to the console.
-    
+
     thread::sleep(tsn);
 
     let tsn2 = Duration::from_micros(1);
     // Print text to the console.
-    
+
     thread::sleep(tsn2);
 
     let tsn3 = Duration::from_millis(1);
     // Print text to the console.
-    
+
+    thread::sleep(tsn3);
+}
+
+pub fn wait_for_a_minute() {
+    let tsn3 = Duration::from_secs(60);
+    // Print text to the console.
+
     thread::sleep(tsn3);
 }
 
 pub fn display_time_elapsed_nice(recorded_start: u128) {
-    println!("Time elapsed since received timestamp: {}", (SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos() - recorded_start)
-    .group_with_nothing()
-    );                                       // this is great
+    println!(
+        "Nanoseconds elapsed since received timestamp: {}",
+        (SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_nanos()
+            - recorded_start)
+            .group_with_nothing()
+    ); // this is great
 }
--- src/monolith.rs
+++ src/monolith.rs
@@ -5,16 +5,254 @@
 // This is the main function.
 
 mod display_mods;
-use display_mods::{Groupable, wait_one_millis_and_micros_and_nanos, display_time_elapsed_nice, record_nanos};
+use display_mods::{
+    display_time_elapsed_nice, record_nanos, wait_one_millis_and_micros_and_nanos, Groupable,
+};
+
+use core::ptr::{null, null_mut};
 
 fn main() {
     let duration_since_epoch_nanos = record_nanos();
     // Statements here are executed when the compiled binary is called.
 
-    let warning_test = "unused";
+    // let warning_test = "unused"; // results in CI warning
 
-    println!("welcome to a {} nanoseconds of runtime", 100000000.group_with_nothing());
+    println!(
+        "Welcome to a {} nanoseconds of runtime",
+        100000000.group_with_nothing()
+    );
 
+    ///|||\\\///|||\\\///|||\\\///|||\\\///|||\\\///|||\\\[ main Main main ]///|||\\\///|||\\\///|||\\\///|||\\\///|||\\\///|||\\\
+    ///|||\\\
+    ///|||\\\
+    ///|||\\\
+    let hInstance = unsafe { GetModuleHandleW(core::ptr::null()) }; // unsafe good
+    let sample_window_class_wn = wide_null("Sample Window Class");
+
+    let mut wc = WNDCLASSW::default();
+    wc.lpfnWndProc = Some(DefWindowProcW);
+    wc.hInstance = hInstance;
+    wc.lpszClassName = sample_window_class_wn.as_ptr();
+
+    let atom = unsafe { RegisterClassW(&wc) }; // smash
+    if atom == 0 {
+        let last_error = unsafe { GetLastError() };
+        panic!(
+            "Could not register the window class, error code: {}",
+            last_error
+        );
+    }
+
+    let sample_window_name_wn = wide_null("Sample Window Name");
+    let hwnd = unsafe {
+        CreateWindowExW(
+            0,
+            sample_window_class_wn.as_ptr(),
+            sample_window_name_wn.as_ptr(),
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            core::ptr::null_mut(),
+            core::ptr::null_mut(),
+            hInstance,
+            core::ptr::null_mut(),
+        )
+    };
+    let _previously_visible = unsafe {
+        ShowWindow(hwnd, SW_SHOW);
+    };
+    if hwnd.is_null() {
+        panic!("Failed to create a window.");
+    }
+
+    let mut msg = MSG::default();
+    loop {
+        let message_return = unsafe { GetMessageW(&mut msg, null_mut(), 0, 0) };
+        if message_return == 0 {
+            break;
+        } else if message_return == -1 {
+            let last_error = unsafe { GetLastError() };
+            panic!("Error with `GetMessageW`, error code: {}", last_error);
+        } else {
+            unsafe {
+                TranslateMessage(&msg);
+                DispatchMessageW(&msg);
+            }
+        }
+    }
+    ///\\\|||///
+    ///\\\|||///
+    ///\\\|||///
+    ///\\\|||///\\\|||///\\\|||///\\\|||///\\\|||///[ the end of main Main main ]\\\|||///\\\|||///\\\|||///\\\|||///\\\|||///\\\|||///
     wait_one_millis_and_micros_and_nanos();
-    display_time_elapsed_nice(duration_since_epoch_nanos);
+    return display_time_elapsed_nice(duration_since_epoch_nanos);
+}
+
+impl Default for WNDCLASSW {
+    #[inline]
+    #[must_use]
+    fn default() -> Self {
+        unsafe { core::mem::zeroed() }
+    }
+}
+
+macro_rules! unsafe_impl_default_zeroed {
+    ($t:ty) => {
+        impl Default for $t {
+            #[inline]
+            #[must_use]
+            fn default() -> Self {
+                unsafe { core::mem::zeroed() }
+            }
+        }
+    };
+}
+
+pub fn wide_null(s: &str) -> Vec<u16> {
+    s.encode_utf16().chain(Some(0)).collect()
+}
+
+type c_int = i32;
+type c_uint = u32;
+type HANDLE = PVOID;
+type HBRUSH = HANDLE;
+type HCURSOR = HICON;
+type HICON = HANDLE;
+type HINSTANCE = HANDLE;
+type HMODULE = HANDLE;
+type HWND = HANDLE;
+type LONG_PTR = isize;
+type LPARAM = LONG_PTR;
+type LPCWSTR = *const WCHAR;
+type LRESULT = LONG_PTR;
+type PVOID = *mut core::ffi::c_void;
+type UINT = c_uint;
+type UINT_PTR = usize;
+type WCHAR = wchar_t;
+type wchar_t = u16;
+type WPARAM = UINT_PTR;
+
+type WNDPROC = Option<
+    unsafe extern "system" fn(hwnd: HWND, uMsg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT,
+>;
+
+#[repr(C)]
+pub struct WNDCLASSW {
+    style: UINT,
+    lpfnWndProc: WNDPROC,
+    cbClsExtra: c_int,
+    cbWndExtra: c_int,
+    hInstance: HINSTANCE,
+    hIcon: HICON,
+    hCursor: HCURSOR,
+    hbrBackground: HBRUSH,
+    lpszMenuName: LPCWSTR,
+    lpszClassName: LPCWSTR,
+}
+
+#[link(name = "Kernel32")]
+extern "system" {
+    /// [`GetModuleHandleW`](https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulehandlew)
+    pub fn GetModuleHandleW(lpModuleName: LPCWSTR) -> HMODULE;
+}
+
+type ATOM = WORD;
+type WORD = c_ushort;
+type c_ushort = u16;
+#[link(name = "User32")]
+extern "system" {
+    /// [`RegisterClassW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassw)
+    pub fn RegisterClassW(lpWndClass: *const WNDCLASSW) -> ATOM;
+}
+
+type DWORD = c_ulong;
+type c_ulong = u32;
+#[link(name = "Kernel32")]
+extern "system" {
+    /// [`GetLastError`](https://docs.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-getlasterror)
+    pub fn GetLastError() -> DWORD;
+}
+
+type HMENU = HANDLE;
+type LPVOID = *mut core::ffi::c_void;
+#[link(name = "User32")]
+extern "system" {
+    /// [`CreateWindowExW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexw)
+    pub fn CreateWindowExW(
+        dwExStyle: DWORD,
+        lpClassName: LPCWSTR,
+        lpWindowName: LPCWSTR,
+        dwStyle: DWORD,
+        X: c_int,
+        Y: c_int,
+        nWidth: c_int,
+        nHeight: c_int,
+        hWndParent: HWND,
+        hMenu: HMENU,
+        hInstance: HINSTANCE,
+        lpParam: LPVOID,
+    ) -> HWND;
+}
+
+const WS_OVERLAPPED: u32 = 0x00000000;
+const WS_CAPTION: u32 = 0x00C00000;
+const WS_SYSMENU: u32 = 0x00080000;
+const WS_THICKFRAME: u32 = 0x00040000;
+const WS_MINIMIZEBOX: u32 = 0x00020000;
+const WS_MAXIMIZEBOX: u32 = 0x00010000;
+const WS_OVERLAPPEDWINDOW: u32 =
+    WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+const CW_USEDEFAULT: c_int = 0x80000000_u32 as c_int;
+
+const SW_SHOW: c_int = 5;
+type BOOL = c_int;
+#[link(name = "User32")]
+extern "system" {
+    /// [`ShowWindow`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow)
+    pub fn ShowWindow(hWnd: HWND, nCmdShow: c_int) -> BOOL;
+}
+
+#[link(name = "User32")]
+extern "system" {
+    /// [`DefWindowProcW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-defwindowprocw)
+    pub fn DefWindowProcW(hWnd: HWND, Msg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT;
+}
+
+#[repr(C)]
+pub struct MSG {
+    hwnd: HWND,
+    message: UINT,
+    wParam: WPARAM,
+    lParam: LPARAM,
+    time: DWORD,
+    pt: POINT,
+    lPrivate: DWORD,
+}
+unsafe_impl_default_zeroed!(MSG);
+
+type LONG = c_long;
+type LPMSG = *mut MSG;
+type c_long = i32;
+#[repr(C)]
+pub struct POINT {
+    x: LONG,
+    y: LONG,
+}
+unsafe_impl_default_zeroed!(POINT);
+
+#[link(name = "User32")]
+extern "system" {
+    /// [`GetMessageW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getmessagew)
+    pub fn GetMessageW(lpMsg: LPMSG, hWnd: HWND, wMsgFilterMin: UINT, wMsgFilterMax: UINT) -> BOOL;
+}
+
+#[link(name = "User32")]
+extern "system" {
+    /// [`TranslateMessage`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-translatemessage)
+    pub fn TranslateMessage(lpMsg: *const MSG) -> BOOL;
+
+    /// [`DispatchMessageW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-dispatchmessagew)
+    pub fn DispatchMessageW(lpMsg: *const MSG) -> LRESULT;
 }
